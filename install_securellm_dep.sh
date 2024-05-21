a0f5a6834c74a946afa76963bcfff984b2125dd6
scuwr <cmarnold@mit.edu>
2024-05-21 15:10:01 -0400

    removed tensor_parallel for anonymous submission

--- install_securellm_dep.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-conda create --name secureLLM python=3.11.4 -y
-
-conda activate secureLLM
-conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia -y
-conda install pip -y
-
-pip install transformers
