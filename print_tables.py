5891b7232587ccb9c7193bb6d4f6faf2ced651db
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-05-21 21:37:35 -0400

    updated printtables with bold

--- print_tables.py
+++ print_tables.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pathlib import Path
+import re
 
 def parse_log(log_path):
     with open(log_path) as f:
@@ -19,17 +20,19 @@ def parse_log(log_path):
     return results
 
 
-def generate_table(table_num, round_digits=1, maincol='tree-norm', show_accuracy=True, show_sql=True):
+def generate_table(table_num, round_digits=1, maincol='tree-norm', acc_cols=[], print_sql=True):
     # table table_num
     print('------------- GENERATING TABLE', table_num, '-------------')
     allpaths = list(Path('.').glob(f'logs/T{table_num}.*.log'))
     if not allpaths:
         print(f'WARNING: No logs found for table {table_num} Skipping table {table_num}...\n'*30)
         return
+    print('Found', len(allpaths), 'logs')
     df = pd.concat([parse_log(log) for log in allpaths])
+    df = df.reset_index(drop=True)
 
     # keep only maincol
-    new_df = df[['name', 'dataset', maincol]]
+    new_df = df[['name', 'dataset', maincol]].copy()
     new_df.loc[:, maincol] = new_df[maincol].astype(float)
 
     # mean and std for each model
@@ -39,22 +42,30 @@ def generate_table(table_num, round_digits=1, maincol='tree-norm', show_accuracy
     mean_std.loc[:, 'std'] = mean_std['std'].astype(float).round(2)
     mean_std['mean_std'] = mean_std['mean'].astype(str) + ' \pm ' + mean_std['std'].astype(str)
 
-    new_df.loc[:, maincol] = new_df[maincol].astype(float).round(round_digits)
-    if show_accuracy:
-        # for accuracy >5%
-        # show_acc_for = df['acc'].astype(float) > 5.0
-        # for all
-        # show_acc_for = df['acc'].astype(float) > -1
-        # for first two columns
-        show_acc_for = df['name'].str.contains('C1|C2')
-        new_df.loc[show_acc_for, maincol] = new_df.loc[show_acc_for, maincol].astype(str) + ' (' + df.loc[show_acc_for, 'acc'] + ')'
+    new_df.loc[:, maincol] = new_df[maincol].astype(float).round(round_digits).astype(str)
+    if acc_cols:
+        # show_acc_for = df['acc'].astype(float) > 5.0  # for accuracy >5%
+        show_acc_for = df['name'].str.contains('|'.join(acc_cols))
+
+        accs = df.loc[show_acc_for, 'acc'].astype(float).round(1).astype(str)
+        new_df.loc[show_acc_for, maincol] = new_df.loc[show_acc_for, maincol] + ' (' + accs + ')'
     new_df.loc[:, 'dataset'] = new_df['dataset'].str.replace('schemapseudo', 'schema').str.replace('_val', '').str.replace('schema_', 'S').str.replace('union', '').str.replace('_gpt', '') # rename datasets to just S#
     new_df = new_df.pivot(index='dataset', columns='name', values=maincol)
     to_sort = ['S1', 'S2', 'S3', 'S12', 'S13', 'S23', 'S123']  # sort rows by this order
     new_df = new_df.reindex(to_sort)
+    if COLS_TO_BOLD:  # find minimums for row and bold them
+        min_values = new_df.copy()
+        piped = '|'.join(COLS_TO_BOLD)
+        min_values = min_values.loc[:, min_values.columns.str.contains(piped)]  # dont calculate minimums over c1 or c2
+        min_values = min_values.apply(lambda x: x.str.extract(r'(.*?)[ $]')[0].astype(float), axis=1).min(axis=1)
+        for dname in new_df.index:
+            min_val = min_values[dname]
+            # cols that contained piped
+            cols = new_df.columns[new_df.columns.str.contains(piped)]
+            new_df.loc[dname, cols] = new_df.loc[dname, cols].apply(lambda x: f'\\textbf{{{x}}}' if re.match(rf'{min_val}[ $]', x) else x)
     new_df.loc['mu +- std'] = mean_std['mean_std'].to_dict()  # new row for 'mu +- std'
     print(new_df)
-    if show_sql:
+    if print_sql:
         print('----------- SQL -----------')
         print(new_df.to_latex())
     new_df.to_csv(f'logs/table{table_num}.csv', index=False)
@@ -62,10 +73,14 @@ def generate_table(table_num, round_digits=1, maincol='tree-norm', show_accuracy
 
 
 
+# COLS_TO_BOLD = []
+COLS_TO_BOLD = ['C3', 'C4', 'C5', 'C6', 'C7']
 
-SHOW_SQL = True
-SHOW_ACCURACY = True
+PRINT_SQL = True
 
-generate_table(table_num=1, show_accuracy=SHOW_ACCURACY, round_digits=1, maincol='tree-norm', show_sql=SHOW_SQL)
-generate_table(table_num=2, show_accuracy=SHOW_ACCURACY, round_digits=1, maincol='tree-norm', show_sql=SHOW_SQL)
-generate_table(table_num=3, show_accuracy=SHOW_ACCURACY, round_digits=1, maincol='tree-norm', show_sql=SHOW_SQL)
+COLUMNS_SHOW_ACCURACY = ['C1', 'C2']
+# COLUMNS_SHOW_ACCURACY = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7']
+
+generate_table(table_num=1, acc_cols=COLUMNS_SHOW_ACCURACY, round_digits=1, maincol='tree-norm', print_sql=PRINT_SQL)
+generate_table(table_num=2, acc_cols=COLUMNS_SHOW_ACCURACY, round_digits=1, maincol='tree-norm', print_sql=PRINT_SQL)
+generate_table(table_num=3, acc_cols=COLUMNS_SHOW_ACCURACY, round_digits=1, maincol='tree-norm', print_sql=PRINT_SQL)
---
+++ run_allcuda1.sh
@@ -0,0 +1,41 @@
+mkdir logs
+
+echo "exposing only cuda 1 to huggingface"
+export CUDA_VISIBLE_DEVICES=1
+
+echo For Table 1 results:
+
+# echo first two columns:
+# python experiments.py --device 0 --sample_size 120 --mroot "./trained_models/SQL/" --models M123E  > logs/T1.C1.log
+# python experiments.py --device 0 --sample_size 120 --mroot "./trained_models/SQL/" --models M123  > logs/T1.C2.log
+# echo column 3-7:
+# python experiments.py --device 0 --sample_size 120 --mroot "./trained_models/SQL/" --models M1 M2 M3 --config SloraLoraHub  >  logs/T1.C3.log
+# python experiments.py --device 0 --sample_size 120 --mroot "./trained_models/SQL/" --models M1 M2 M3 --config SloraSum  >  logs/T1.C4.log
+# python experiments.py --device 0 --sample_size 120 --mroot "./trained_models/SQL/" --models M1 M2 M3 --config SloraMax  >  logs/T1.C5.log
+python experiments.py --device 0 --sample_size 120 --mroot "./trained_models/SQL/" --models M1 M2 M3 --config SloraLogit  >  logs/T1.C6.log
+python experiments.py --device 0 --sample_size 120 --mroot "./trained_models/6NF/" --pseudo --models M1 M2 M3 --config SloraLogit  >  logs/T1.C7.log
+
+echo For Table 2 results:
+
+echo first two columns:
+python experiments.py --device 0 --sample_size 120 --gpt --mroot "./trained_models/SQL/" --models M123E  > logs/T2.C1.log
+python experiments.py --device 0 --sample_size 120 --gpt --mroot "./trained_models/SQL/" --models M123  > logs/T2.C2.log
+echo column 3-7:
+python experiments.py --device 0 --sample_size 120 --gpt --mroot "./trained_models/SQL/" --models M1 M2 M3 --config SloraLoraHub  >  logs/T2.C3.log
+# python experiments.py --device 0 --sample_size 120 --gpt --mroot "./trained_models/SQL/" --models M1 M2 M3 --config SloraSum  >  logs/T2.C4.log
+# python experiments.py --device 0 --sample_size 120 --gpt --mroot "./trained_models/SQL/" --models M1 M2 M3 --config SloraMax  >  logs/T2.C5.log
+# python experiments.py --device 0 --sample_size 120 --gpt --mroot "./trained_models/SQL/" --models M1 M2 M3 --config SloraLogit  >  logs/T2.C6.log
+# python experiments.py --device 0 --sample_size 120 --gpt --mroot "./trained_models/6NF/" --pseudo --models M1 M2 M3 --config SloraLogit  >  logs/T2.C7.log
+
+# echo For Table 3 results:
+
+# echo first two columns:
+# python experiments.py --device 0 --sample_size 120 --mapping 1 --mroot "./trained_models/SQL_obf/" --models M123E  > logs/T3.C1.log
+# python experiments.py --device 0 --sample_size 120 --mapping 1 --mroot "./trained_models/SQL_obf/" --models M123  > logs/T3.C2.log
+# echo column 3-7:
+# python experiments.py --device 0 --sample_size 120 --mapping 1 --mroot "./trained_models/SQL_obf/" --models M1 M2 M3 --config SloraLoraHub  >  logs/T3.C3.log
+# python experiments.py --device 0 --sample_size 120 --mapping 1 --mroot "./trained_models/SQL_obf/" --models M1 M2 M3 --config SloraSum  >  logs/T3.C4.log
+# python experiments.py --device 0 --sample_size 120 --mapping 1 --mroot "./trained_models/SQL_obf/" --models M1 M2 M3 --config SloraMax  >  logs/T3.C5.log
+# python experiments.py --device 0 --sample_size 120 --mapping 1 --mroot "./trained_models/SQL_obf/" --models M1 M2 M3 --config SloraLogit  >  logs/T3.C6.log
+# python experiments.py --device 0 --sample_size 120 --mapping 1 --mroot "./trained_models/6NF_obf/" --pseudo --models M1 M2 M3 --config SloraLogit  >  logs/T3.C7.log
+
